cc93239c671f116c90355de99710d1269312c1bd
zangura10 <g.zangurashvili@optimobet.com>
2024-07-17 21:18:46 +0400

    Revert "Update project"
    This reverts commit 0074ada5cbdf22d3be6add9aca13cfa4f2cc42c3.

--- oauth2-authorization-server/src/main/java/oauth2/authorization/server/exception/ClientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package oauth2.authorization.server.exception;
-
-public class ClientNotFoundException extends RuntimeException {
-    public ClientNotFoundException(String message) {
-        super(message);
-    }
-}
